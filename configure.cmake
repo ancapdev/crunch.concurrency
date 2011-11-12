a8145314c409f659cfb5f548aebebef5fa94eea7
Christian Rorvik <christian.rorvik@gmail.com>
2011-11-12 22:08:46 +0000

    Conversion to package format

---
+++ configure.cmake
@@ -0,0 +1,10 @@
+# Copyright (c) 2011, Christian Rorvik
+# Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
+
+vpm_depend(
+  crunch_base
+  crunch_benchmarking
+  crunch_containers
+  crunch_test)
+
+include_directories(${CMAKE_CURRENT_LIST_DIR}/include)

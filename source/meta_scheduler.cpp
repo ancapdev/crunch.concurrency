80a17ec63ff3b1637d0d1960372a9a509964bd02
Christian Rorvik <christian.rorvik@gmail.com>
2012-08-13 20:23:12 +0100

    Fixed bug where one idle scheduler could cause the MetaScheduler not to advance its round robin schedule

--- source/meta_scheduler.cpp
+++ source/meta_scheduler.cpp
@@ -433,6 +433,7 @@ public:
                     else
                     {
                         ss->lastState = ISchedulerContext::State::Working;
+                        ++it;
                     }
                 }
                 else
